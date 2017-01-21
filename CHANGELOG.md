f0c438b59c33ad30f99a4bb6492e36064d5f0212
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-01-21 10:03:58 +0100

    0.1.1

--- CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-### 0.1.1
-
-[[#2](https://github.com/VadimDez/ng2-order-pipe/issues/2)] - Add param to reverse array
--- index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Created by vadimdez on 20/01/2017.
- */
-export * from './src/ng2-order.module';
-export * from './src/ng2-order.pipe';
