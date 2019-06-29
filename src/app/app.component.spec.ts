50b10991e987171dcf51e78055a4700c90b77240
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2019-06-29 17:53:54 +0200

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
+import { OrderModule } from "./order-pipe/ngx-order.module";
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      declarations: [AppComponent],
+      imports: [OrderModule]
     }).compileComponents();
   }));
 
@@ -14,18 +16,12 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngx-order-pipe'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("ngx-order-pipe");
-  });
-
   it("should render title in a h1 tag", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain(
-      "Welcome to ngx-order-pipe!"
+    expect(compiled.querySelector(".mdl-layout-title").textContent).toContain(
+      "ngx-order-pipe"
     );
   });
 });
